4b440c6340083f32de26bb12436342d972e9ab91
Shunyu Zhao <shunyu2@ualberta.ca>
2023-02-16 13:43:01 -0700

    connected project to firestore database with quick test to check if succcessful

--- app/app/src/main/java/com/example/scavengingscannables/ui/home/HomeViewModel.java
+++ app/app/src/main/java/com/example/scavengingscannables/ui/home/HomeViewModel.java
@@ -1,8 +1,15 @@
 package com.example.scavengingscannables.ui.home;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeViewModel extends ViewModel {
 
@@ -10,7 +17,23 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+
+        // quick test code to check if database works
+        // should see {TestField=TestValue} instead of "This is home fragment" if successful
+        FirebaseFirestore db;
+        db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("TestCollection").document("TestID");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    mText.setValue(String.valueOf(document.getData()));
+                }else{
+                    mText.setValue("This is home fragment");
+                }
+            }
+        });
     }
 
     public LiveData<String> getText() {
