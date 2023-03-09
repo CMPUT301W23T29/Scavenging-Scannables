58c002942ab090962b8da1199f0055fa2a2379e0
DarrenKrz <123832313+DarrenKrz@users.noreply.github.com>
2023-03-08 18:49:37 -0700

    Changed use of arraylist to list

--- app/app/src/main/java/com/example/scavengingscannables/QrInfo.java
+++ app/app/src/main/java/com/example/scavengingscannables/QrInfo.java
@@ -3,16 +3,14 @@ package com.example.scavengingscannables;
 public class QrInfo {
     // Will generate a visual based on the qrID
     public String generateVisual(Integer qrID) {
-        String visualization;
+        String visualization = "";
         int hashOfQRID = qrID.hashCode();
 
         // Checks first 6 bits of HashCode to determine visualization, currently using placeholders
         // as visualizations
 
-        ArrayList<String> VisualOptionA = new ArrayList<String>;
-        ArrayList<String> VisualOptionB = new ArrayList<String>;
-        VisualOptionA.addAll("1stOptionA\n", "2ndOptionA\n","3rdOptionA\n", "4thOptionA\n","5thOptionA\n", "6thOptionA\n",);
-        VisualOptionA.addAll("1stOptionB\n", "2ndOptionB\n","3rdOptionB\n", "4thOptionB\n","5thOptionB\n", "6thOptionB\n",);
+        String[] VisualOptionA = {"1stOptionB\n", "2ndOptionB\n","3rdOptionB\n", "4thOptionB\n","5thOptionB\n", "6thOptionB\n",};
+        String[] VisualOptionB = {"1stOptionA\n","2ndOptionA\n","3rdOptionA\n", "4thOptionA\n","5thOptionA\n", "6thOptionA\n",};
 
         for (int i = 0; i < 6; i++) {
             if (hashOfQRID << i == 0) {
