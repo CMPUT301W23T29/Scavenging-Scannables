58c5ab81d614cf842b4d315cdb0f5e567e9fee9a
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-03-08 21:52:13 -0700

    Added finish call to end activity once code is scanned

--- app/app/src/main/java/com/example/scavengingscannables/ScannerActivity.java
+++ app/app/src/main/java/com/example/scavengingscannables/ScannerActivity.java
@@ -44,6 +44,7 @@ public class ScannerActivity extends AppCompatActivity {
 
                         fdc.SaveQRCodeByID(newCode);
 
+                        finish();
                     }
                 });
             }
