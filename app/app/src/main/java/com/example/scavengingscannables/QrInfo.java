a99e6f58faeb252ead9f6e6160a0cf58b3932457
DarrenKrz <123832313+DarrenKrz@users.noreply.github.com>
2023-03-09 10:18:45 -0700

    Updated QrInfo class to get hash input, created new method for generating a visual

--- app/app/src/main/java/com/example/scavengingscannables/QrInfo.java
+++ app/app/src/main/java/com/example/scavengingscannables/QrInfo.java
@@ -1,23 +1,22 @@
 package com.example.scavengingscannables;
 
+import java.lang.Object;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.img.display.imagej.ImageJFunctions; // not sure why this won't import
+
 public class QrInfo {
-    // Will generate a visual based on the qrID
-    public String generateVisual(Integer qrID) {
-        String visualization = "";
-        int hashOfQRID = qrID.hashCode();
-
-        // Each hexa input corresponds to a different element on the visualization, use only first 6 hexa characters
-
-        String[] VisualOptionA = {"1stOptionB\n", "2ndOptionB\n","3rdOptionB\n", "4thOptionB\n","5thOptionB\n", "6thOptionB\n",};
-        String[] VisualOptionB = {"1stOptionA\n","2ndOptionA\n","3rdOptionA\n", "4thOptionA\n","5thOptionA\n", "6thOptionA\n",};
-
-        for (int i = 0; i < 6; i++) {
-            if (hashOfQRID << i == 0) {
-                visualization = VisualOptionA[i];
-            } else {
-                visualization = VisualOptionB[i];
-            }
-        }
-        return visualization;
+    // Will generate a visualizationCode based on the hash of qrID
+    public String GenerateVisualCode(String hash) {
+        // Each hexa input corresponds to a different element on the visualization, use only first 6 characters
+        //visualization code can then be used to generate a unique image using image generator
+        String visualizationCode =hash.substring(0,6);
+        return visualizationCode;
+    }
+    public void generateVisual(String visualizationCode) {
+        //Will generate an image based on the code given, currently just testing
+        final Img<UnsignedByteType> visual = new ArrayImgFactory< UnsignedByteType>().create(new long[] {400,320}, new UnsignedByteType());
+        ImageJFunctions.show(visual);
     }
 }
