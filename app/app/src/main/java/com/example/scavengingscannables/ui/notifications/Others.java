63ad0b894ee7b7ea2c0af16059bdd4c5bdf2429b
Zhi Liu <zhi2@ualberta.ca>
2023-03-11 13:24:47 -0700

    Click others to see people who scan same qrcode

--- app/app/src/main/java/com/example/scavengingscannables/ui/notifications/Others.java
+++ app/app/src/main/java/com/example/scavengingscannables/ui/notifications/Others.java
@@ -2,8 +2,12 @@ package com.example.scavengingscannables.ui.notifications;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +17,14 @@ import com.example.scavengingscannables.Player;
 import com.example.scavengingscannables.QrCode;
 import com.example.scavengingscannables.R;
 
+import java.util.ArrayList;
+
 public class Others extends AppCompatActivity {
 
     Integer QrCodeID;
     Button backButton;
+    ArrayList<String> playerNamesList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +32,14 @@ public class Others extends AppCompatActivity {
         setContentView(R.layout.activity_display_others);
         Intent intent = getIntent();
         QrCodeID = intent.getIntExtra("QrCodeID",0);
+
         FirestoreDatabaseController dbc = new FirestoreDatabaseController();
         backButton = findViewById(R.id.button_others_back);
+        ListView playerNamesView = findViewById(R.id.others_list);
+        playerNamesList = new ArrayList<>();
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this, R.layout.playername,playerNamesList);
+        playerNamesView.setAdapter(arrayAdapter);
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -33,12 +47,25 @@ public class Others extends AppCompatActivity {
             }
         });
 
-        dbc.GetQRCodeByID(QrCodeID, new FirestoreDatabaseCallback() {
+        dbc.GetAllUsernames(new FirestoreDatabaseCallback() {
             @Override
             public <T> void OnDataCallback(T data) {
-                QrCode q = (QrCode) data;
+                ArrayList<String> usernames = (ArrayList<String>) data;
+                for (String userName : usernames){
+                    dbc.GetPlayerByUsername(userName, new FirestoreDatabaseCallback() {
+                        @Override
+                        public <T> void OnDataCallback(T data) {
+                            Player p = (Player)data;
+                            if (p.getScannedQRCodesID().contains(QrCodeID)){
+                                playerNamesList.add(userName);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    });
 
+                }
             }
         });
+
     }
 }
