579c5029be3cb94acc5aa06c34913f406e73d851
deqwaad <73512318+deqwaad@users.noreply.github.com>
2023-03-12 17:38:30 -0600

    updating test of FindUser Function

--- app/app/src/androidTest/java/com/example/scavengingscannables/FindUserTest.java
+++ app/app/src/androidTest/java/com/example/scavengingscannables/FindUserTest.java
@@ -14,6 +14,7 @@ import com.example.scavengingscannables.ui.home.FindUserActivity;
 import com.example.scavengingscannables.ui.home.HomeFragment;
 import com.example.scavengingscannables.ui.home.HomeViewModel;
 import com.example.scavengingscannables.ui.home.OthersQrCodesActivity;
+import com.example.scavengingscannables.ui.notifications.QrCodesActivity;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -25,19 +26,31 @@ public class FindUserTest {
     private Solo solo;
     private TextView username;
     @Rule
-    public ActivityTestRule rule = new ActivityTestRule<>(FindUserActivity.class, true,true);
+    public ActivityTestRule rule = new ActivityTestRule<>(MainActivity.class, true,true);
     @Before
     public void setUp() throws Exception{
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
     @Test
     public void check(){
-        solo.assertCurrentActivity("Wrong Activity", FindUserActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity",FindUserActivity.class);
         solo.enterText((EditText) solo.getView(R.id.search_box_input),"er");
         solo.clickOnButton("Search");
         solo.waitForText("er", 1, 2000);
         solo.clickInList(0);
+        solo.clickOnView(solo.getView(R.id.display_user_name));
         solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+        solo.clickOnView(solo.getView(R.id.view_qrCodes));
+        solo.assertCurrentActivity("Wrong Activity", OthersQrCodesActivity.class);
+        solo.clickOnView(solo.getView(R.id.other_back_button));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+        solo.clickOnView(solo.getView(R.id.display_back));
+        solo.assertCurrentActivity("Wrong Activity",FindUserActivity.class);
+        solo.clickOnView(solo.getView(R.id.search_back_button));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
     }
     @After
     public void tearDown()throws Exception{
