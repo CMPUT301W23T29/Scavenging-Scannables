a32f8a6089965b5ffe5ccb3be2e90d162b32220c
deqwaad <73512318+deqwaad@users.noreply.github.com>
2023-03-12 18:30:43 -0600

    improve test of finduser

--- app/app/src/androidTest/java/com/example/scavengingscannables/FindUserTest.java
+++ app/app/src/androidTest/java/com/example/scavengingscannables/FindUserTest.java
@@ -32,7 +32,87 @@ public class FindUserTest {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
     @Test
-    public void check(){
+    public void check_imagebutton(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity",FindUserActivity.class);
+    }
+    @Test
+    public void check_edittest_and_searchbutoon() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity", FindUserActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.search_box_input), "er");
+        solo.clickOnButton("Search");
+        solo.waitForText("er", 1, 2000);
+    }
+    @Test
+    public void check_Switching_From_FindUserActivity_to_DisplaySearchActivity() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity", FindUserActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.search_box_input), "er");
+        solo.clickOnButton("Search");
+        solo.waitForText("er", 1, 2000);
+        solo.clickInList(0);
+        solo.clickOnView(solo.getView(R.id.display_user_name));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+    }
+    @Test
+    public void check_Switching_From_DisplaySearch_to_OtherQRCodes() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity", FindUserActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.search_box_input), "er");
+        solo.clickOnButton("Search");
+        solo.waitForText("er", 1, 2000);
+        solo.clickInList(0);
+        solo.clickOnView(solo.getView(R.id.display_user_name));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+        solo.clickOnView(solo.getView(R.id.view_qrCodes));
+        solo.assertCurrentActivity("Wrong Activity", OthersQrCodesActivity.class);
+    }
+    @Test
+    public void check_backbutton_of_OtherQrCodes() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity", FindUserActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.search_box_input), "er");
+        solo.clickOnButton("Search");
+        solo.waitForText("er", 1, 2000);
+        solo.clickInList(0);
+        solo.clickOnView(solo.getView(R.id.display_user_name));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+        solo.clickOnView(solo.getView(R.id.view_qrCodes));
+        solo.assertCurrentActivity("Wrong Activity", OthersQrCodesActivity.class);
+        solo.clickOnView(solo.getView(R.id.other_back_button));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+    }
+    @Test
+    public void check_backbutton_of_DisplaySearch() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        solo.clickOnView(solo.getView(R.id.search_button1));
+        solo.assertCurrentActivity("Wrong Activity", FindUserActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.search_box_input), "er");
+        solo.clickOnButton("Search");
+        solo.waitForText("er", 1, 2000);
+        solo.clickInList(0);
+        solo.clickOnView(solo.getView(R.id.display_user_name));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+        solo.clickOnView(solo.getView(R.id.view_qrCodes));
+        solo.assertCurrentActivity("Wrong Activity", OthersQrCodesActivity.class);
+        solo.clickOnView(solo.getView(R.id.other_back_button));
+        solo.assertCurrentActivity("Wrong Activity", DisplaySearch.class);
+        solo.clickOnView(solo.getView(R.id.display_back));
+    }
+    @Test
+    public void check_button_of_FindUser(){
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.navigation_home));
         solo.clickOnView(solo.getView(R.id.search_button1));
